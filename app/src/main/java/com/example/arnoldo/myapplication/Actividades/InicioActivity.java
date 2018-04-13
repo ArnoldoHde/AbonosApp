735d6585e606e9bbbdec8a2fa17725ce6eb45443
Unknown <sauceda8888@gmail.com>
2018-04-13 12:57:01 -0600

    Creacion del menu de navegacion lateral
    Se creo el menu lateral de navegacion para editar perfil o cerrar sesion

--- app/src/main/java/com/example/arnoldo/myapplication/Actividades/InicioActivity.java
+++ app/src/main/java/com/example/arnoldo/myapplication/Actividades/InicioActivity.java
@@ -1,11 +1,17 @@
 package com.example.arnoldo.myapplication.Actividades;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.arnoldo.myapplication.Fragmentos.fragment_clientes;
@@ -13,8 +19,10 @@ import com.example.arnoldo.myapplication.Fragmentos.fragment_inicio;
 import com.example.arnoldo.myapplication.Fragmentos.fragment_pedidos;
 import com.example.arnoldo.myapplication.R;
 
-public class inicioActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    //Se crea el menú de Navegacion entre Fragments "Inicio, Clientes y Pedidos"
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -39,13 +47,57 @@ public class inicioActivity extends AppCompatActivity {
             }
             return false;
         }
-    };@Override
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.drawerEdit) {
+            // Handle the camera action
+        } else if (id == R.id.drawerCerrarSesion) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 }
