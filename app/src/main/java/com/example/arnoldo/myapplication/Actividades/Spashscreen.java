28ca55dd9dd1a63258278b3c00f0641bd9f724cb
Unknown <iruga.dmc@gmail.com>
2018-04-15 19:56:26 -0600

    Splash Screen
    se creo Splash screen con el logo de la app
    (Tomar en cuenta que sera la primera actividad de ejecucón de la app segudo del log in)

---
+++ app/src/main/java/com/example/arnoldo/myapplication/Actividades/Spashscreen.java
@@ -0,0 +1,43 @@
+package com.example.arnoldo.myapplication.Actividades;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.arnoldo.myapplication.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Spashscreen extends AppCompatActivity {
+    // tiempo del splash
+    private static final long Tiempo_del_delay = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //se agrega la orientacion portrait
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //se oculta la barra de titulo
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_spashscreen);
+
+        TimerTask task =new TimerTask() {
+            @Override
+            public void run() {
+                //empesar siguiente actividad
+                Intent mainIntent =new Intent().setClass(Spashscreen.this, InicioActivity.class);
+                startActivity(mainIntent);
+                //cerramos la actividad para que el usuario no pueda regresar a ella
+                finish();
+            }
+        };
+        Timer timer =new Timer();
+        timer.schedule(task,Tiempo_del_delay);
+
+    }
+}
