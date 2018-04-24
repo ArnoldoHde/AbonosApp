8e191fcd92f01eee652061d140cc57c9ba2f05a7
Arnoldo Hernandez <arnoldohernandez4533@gmail.com>
2018-04-24 11:16:26 -0600

    modelo para el pedido

---
+++ app/src/main/java/com/example/arnoldo/myapplication/Managers/PedidoManeger.java
@@ -0,0 +1,8 @@
+package com.example.arnoldo.myapplication.Managers;
+
+/**
+ * Created by arnoldo on 24/04/18.
+ */
+
+public class PedidoManeger {
+}
---
+++ app/src/main/java/com/example/arnoldo/myapplication/Modelos/RegistroPedido.java
@@ -0,0 +1,8 @@
+package com.example.arnoldo.myapplication.Modelos;
+
+/**
+ * Created by arnoldo on 24/04/18.
+ */
+
+public class RegistroPedido {
+}
